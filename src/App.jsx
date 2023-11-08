8f2c0e76823d0638e51c64b0e3c14b78036ebc3f
norristown <jarvasity@gmail.com>
2023-11-08 00:38:05 -0500

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const [storeItems, setStoreItems] = useState([]);
-  useEffect(function () {
-    async function fetchAPI() {
-      try {
-        setIsLoading(true);
-        const res = await fetch("https://dummyjson.com/products");
-
-        if (!res.ok) throw new Error("Something went wrong");
-        const data = await res.json();
-        console.log(data.products);
-        setStoreItems(data.products);
-      } catch (err) {
-        console.log(err);
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
+  const initialState = {
+    data: [],
+    //loading, error, ready, active, finished
+    status: "loading",
+  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return { ...state, data: action.payload };
+      case "dataFailed":
+        return { ...state, status: "Error" };
+      default:
+        throw new Error("action unknown");
     }
-    fetchAPI();
+  }
+  const [{ data, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    // const controller = new AbortController();
+    // async function fetchAPI() {
+    //   try {
+    //     setIsLoading(true);
+    //     const res = await fetch("https://fakestoreapi.com/products",
+    //     // {signal: controller.signal,}
+    //     );
+
+    //     if (!res.ok) throw new Error("Something went wrong");
+    //     const data = await res.json();
+    //     dispatch({ type: "dataReceived", payload: data });
+    //   } catch (err) {
+    //     console.log(err);
+    //     setError(err.message);
+    //   } finally {
+    //     setIsLoading(false);
+    //   }
+    // }
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+    // fetchAPI();
+
+    // return function () {
+    //   controller.abort();
+    // };
   }, []);
   return (
     <>
       {isLoading && <Loader />}
-      {!isLoading && !error && <StorePage storeItems={storeItems} />}
+      {!isLoading && !error && <StorePage data={data} />}
       {error && <ErrorMessage message={error} />}
     </>
   );
 }
 
-function StorePage({ storeItems }) {
+function StorePage({ data }) {
   return (
-    <>
-      {storeItems.map((prev) => (
-        <div key={prev.id}>
-          <p>{prev.id}</p>
-          <p>{prev.brand}</p>
-          <img src={prev.images[0]} />
-        </div>
+    <div>
+      {data.map((x) => (
+        <div key={x.id}>{x.title}</div>
       ))}
-    </>
+    </div>
   );
 }
 
